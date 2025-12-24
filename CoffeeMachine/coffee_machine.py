54e5c3bfc54107ab3fae3883e529a10166402bab
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2025-12-24 00:02:26 -0500

    Coffee Machine: complete Stage 1

---
+++ CoffeeMachine/coffee_machine.py
@@ -0,0 +1,12 @@
+steps = [
+    'Starting to make a coffee',
+    'Grinding coffee beans',
+    'Boiling water',
+    'Mixing boiled water with crushed coffee beans',
+    'Pouring coffee into the cup',
+    'Pouring some milk into the cup',
+    'Coffee is ready!'
+]
+
+for step in steps:
+    print(step)
