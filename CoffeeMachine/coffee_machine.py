3c2aa4f6fe4dacf0242b344827f4484394fca2cd
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2025-12-24 14:32:52 -0500

    Coffee Machine: complete Stage 3

--- CoffeeMachine/coffee_machine.py
+++ CoffeeMachine/coffee_machine.py
@@ -2,9 +2,15 @@ water_per_cup = 200
 milk_per_cup = 50
 beans_per_cup = 15
 
-cups = int(input('Write how many cups of coffee you will need: '))
+water = int(input("Write how many ml of water the coffee machine has: "))
+milk = int(input("Write how many ml of milk the coffee machine has: "))
+beans = int(input("Write how many grams of coffee beans the coffee machine has: "))
+cups_requested = int(input("Write how many cups of coffee you will need: "))
+cups_available = min(water // water_per_cup, milk // milk_per_cup, beans // beans_per_cup)
 
-print(f'''For {cups} cups of coffee you will need:
-{cups * water_per_cup} ml of water
-{cups * milk_per_cup} ml of milk
-{cups * beans_per_cup} g of coffee beans''')
+if cups_available > cups_requested:
+    print(f"Yes, I can make that amount of coffee (and even {cups_available - cups_requested} more than that)")
+elif cups_available == cups_requested:
+    print("Yes, I can make that amount of coffee")
+else:
+    print(f"No, I can make only {cups_available} cups of coffee")
