08ba70b4519fc217714efd310674c654fb66fb60
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2025-12-31 09:02:21 -0500

    Loan Calculator: complete Stage 2

--- LoanCalculator/calculator.py
+++ LoanCalculator/calculator.py
@@ -1,3 +1,23 @@
+from math import ceil
+
 principal = int(input('Enter the loan principal: '))
-payment = int(input('Enter the monthly payment: '))
-print(f'It will take {principal / payment} months to repay the loan')
+
+calculation = input("""What do you want to calculate?
+type "m" - for number of monthly payments,
+type "p" - for the monthly payment: """)
+
+match calculation:
+    case 'm':
+        payment = int(input('Enter the monthly payment: '))
+        months = ceil(principal / payment)
+        print(f'\nIt will take {months} {'months' if months > 1 else 'month'} to repay the loan.')
+    case 'p':
+        months = int(input('Enter the number of months: '))
+        regular = ceil(principal / months)
+        if principal % regular == 0:
+            print(f'\nYour monthly payment = {regular}.')
+        else:
+            final = principal - (months - 1) * regular
+            print(f'\nYour monthly payment = {regular} and the last payment = {final}.')
+    case _:
+        print('\nInvalid response.')
