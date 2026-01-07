b91ad6701f002a91763f91b341619d73f34ee170
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2026-01-06 22:43:18 -0500

    Currency Converter: complete Stage 2

---
+++ CurrencyConverter/solution.py
@@ -0,0 +1,6 @@
+CONICOINS_TO_DOLLARS = 100
+
+conicoins = int(input())
+print('I have', conicoins, 'conicoins.')
+print(f'{conicoins} conicoins cost {conicoins * CONICOINS_TO_DOLLARS} dollars.')
+print('I am rich! Yippee!')
