adb8f0912ed4069fdcec1ca1ce652eeba698e73a
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2026-01-08 11:35:31 -0500

    Currency Converter: complete Stage 6

--- CurrencyConverter/stage6.py
+++ CurrencyConverter/stage6.py
@@ -2,8 +2,31 @@ import json
 
 import requests
 
-currency_code = input()
-req = requests.get('http://www.floatrates.com/daily/' + currency_code.lower() + '.json')
-rates = json.loads(req.text)
-print(rates['usd'])
-print(rates['eur'])
+
+def lookup(rates, source, *targets):
+    req = requests.get('http://www.floatrates.com/daily/' + source.lower() + '.json')
+    res = json.loads(req.text)
+    for target in targets:
+        if source == target:
+            rates[target] = 1
+        else:
+            rates[target] = res[target.lower()]['rate']
+
+
+def main():
+    rates = {}
+    source = input().upper()
+    lookup(rates, source, 'USD', 'EUR')
+    while target := input().upper():
+        amount = float(input())
+        print('Checking the cache...')
+        if rates.get(target):
+            print('Oh! It is in the cache!')
+        else:
+            print('Sorry, but it is not in the cache!')
+            lookup(rates, source, target)
+        print(f'You received {round(amount * (rates[target]), 2)} {target}.')
+
+
+if __name__ == '__main__':
+    main()
