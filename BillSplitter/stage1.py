1383de5040b3ef8b6496035d74f5e1ff448542fe
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2026-01-08 15:11:45 -0500

    Bill Splitter: improve Stage 1 solution
    You can use a dictionary comprehension instead of a loop.

--- BillSplitter/stage1.py
+++ BillSplitter/stage1.py
@@ -2,8 +2,6 @@ n = int(input('Enter the number of friends joining (including you): '))
 if n < 1:
     print('\nNo one is joining for the party')
 else:
-    friends = {}
     print('\nEnter the name of every friend (including you), each on a new line:')
-    for i in range(n):
-        friends[input()] = 0
+    friends = {input(): 0 for _ in range(n)}
     print(f'\n{friends}')
