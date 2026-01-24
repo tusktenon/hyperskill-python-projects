c3e78819e30b2490a8e7cd0071eb196175095549
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2026-01-24 16:52:56 -0500

    Web Scraper: complete Stage 4

---
+++ WebScraper/stage4.py
@@ -0,0 +1,24 @@
+import string
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3'
+response = requests.get(URL)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+for article in soup.find_all('article'):
+    article_type = article.find('span', {'data-test': 'article.type'})
+    if article_type.text == 'News':
+        link = article.find('a', {'data-track-action': 'view article'})
+        link_response = requests.get('https://www.nature.com' + link.get('href'))
+        link_soup = BeautifulSoup(link_response.content, 'html.parser')
+        title = link_soup.find('h1').text.strip()
+        filename = title
+        for p in string.punctuation:
+            filename = filename.replace(p, '')
+        filename = filename.replace(' ', '_')
+        teaser = link_soup.find('p', {'class': 'article__teaser'})
+        print(f'Saving "{title}"...')
+        with open(filename + '.txt', 'w', encoding='utf-8') as file:
+            file.write(teaser.text)
