954010c671ccf9522917b4a0b0e806ab64d73ec7
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2026-01-28 18:28:51 -0500

    UAP: improve Stage 4 solution

--- UniversityAdmissionProcedure/stage4.py
+++ UniversityAdmissionProcedure/stage4.py
@@ -1,32 +1,22 @@
-DEPARTMENTS = ['Biotech', 'Chemistry', 'Engineering', 'Mathematics', 'Physics']
-
 capacity = int(input())
 applicants = set()
 with open('applicants.txt') as file:
     for line in file:
         name, gpa, first, second, third = line.rsplit(maxsplit=4)
         applicants.add((name, float(gpa), first, second, third))
-accepted = [[] for _ in DEPARTMENTS]
+sorted_applicants = sorted(list(applicants), key=lambda x: (-x[1], x[0]))
+accepted = {'Biotech': [], 'Chemistry': [], 'Engineering': [], 'Mathematics': [], 'Physics': []}
 
-for priority in range(3):
-    if not applicants or min([len(a) for a in accepted]) >= capacity:
-        break
-    rankings = [[] for _ in DEPARTMENTS]
-    for applicant in applicants:
-        rankings[DEPARTMENTS.index(applicant[priority + 2])].append(applicant)
-    for department_ranking in rankings:
-        department_ranking.sort(key=lambda x: (-x[1], x[0]))
-    for i in range(len(DEPARTMENTS)):
-        for j in range(min(capacity - len(accepted[i]), len(rankings[i]))):
-            accepted[i].append(rankings[i][j])
-            applicants.remove(rankings[i][j])
+for priority in range(2, 5):
+    for applicant in sorted_applicants:
+        department = applicant[priority]
+        if len(accepted[department]) < capacity and applicant in applicants:
+            accepted[department].append(applicant)
+            applicants.remove(applicant)
 
-# Hyperskil tests require the lists of accepted students for each department
-# to be sorted (by GPA, then name)
-for department_acceptances in accepted:
-    department_acceptances.sort(key=lambda x: (-x[1], x[0]))
-
-for i, department in enumerate(DEPARTMENTS):
+for department, students in accepted.items():
+    # Hyperskill tests require the printed lists of accepted students
+    # for each department to be sorted
+    students.sort(key=lambda x: (-x[1], x[0]))
     print('\n' + department)
-    for student in accepted[i]:
-        print(student[0], student[1])
+    print(*[f'{student[0]} {student[1]}' for student in students], sep='\n')
