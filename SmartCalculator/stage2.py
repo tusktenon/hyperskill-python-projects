7c4b7d54fd46663f812bc6b4c050421766700088
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2026-02-08 17:05:02 -0500

    Smart Calculator: improve Stage 2 solution

--- SmartCalculator/stage2.py
+++ SmartCalculator/stage2.py
@@ -1,11 +1,5 @@
-while True:
-    match input().split():
-        case [a, b]:
-            print(int(a) + int(b))
-        case ['/exit']:
-            print('Bye!')
-            break
-        case [a]:
-            print(a)
-        case _:
-            pass
+while (user_input := input()) != '/exit':
+    if user_input:
+        numbers = map(int, user_input.split())
+        print(sum(numbers))
+print('Bye!')
