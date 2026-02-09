220bdbf8727eddce4e735023f87554c73cc37db2
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2026-02-09 09:26:03 -0500

    Smart Calculator: complete Stage 5

---
+++ SmartCalculator/stage5.py
@@ -0,0 +1,26 @@
+def evaluate(expression):
+    terms = expression.split()
+    while len(terms) > 1:
+        a, op, b, *terms = terms
+        a, b = int(a), int(b)
+        op = int.__add__ if op.count('-') % 2 == 0 else int.__sub__
+        terms.insert(0, op(a, b))
+    return int(terms[0])
+
+
+def main():
+    while (user_input := input()) != '/exit':
+        if user_input == '/help':
+            print('The program calculates sums and differences of any length')
+        elif user_input.startswith('/'):
+            print('Unknown command')
+        elif user_input:
+            try:
+                print(evaluate(user_input))
+            except ValueError:
+                print('Invalid expression')
+    print('Bye!')
+
+
+if __name__ == '__main__':
+    main()
