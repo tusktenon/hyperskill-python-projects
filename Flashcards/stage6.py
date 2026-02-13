0de82c3453794ed4be9706133b257472b386c3f3
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2026-02-13 09:24:45 -0500

    Flashcards: complete Stage 6

--- Flashcards/stage6.py
+++ Flashcards/stage6.py
@@ -5,64 +5,76 @@ from random import choices
 cards = {}
 inverted = {}
 errors = Counter()
+log = []
+
+
+def _console_in():
+    user_input = input()
+    log.append(user_input)
+    return user_input
+
+
+def _console_out(string):
+    print(string)
+    log.append(string)
 
 
 def add_card():
-    print('The card:')
+    _console_out('The card:')
     while (term := input()) in cards:
-        print(f'The card "{term}" already exists. Try again:')
-    print('The definition of the card:')
+        _console_out(f'The card "{term}" already exists. Try again:')
+    _console_out('The definition of the card:')
     while (definition := input()) in inverted:
-        print(f'The definition "{definition}" already exists. Try again:')
+        _console_out(f'The definition "{definition}" already exists. Try again:')
     cards[term] = definition
     inverted[definition] = term
-    print(f'The pair ("{term}":"{definition}") has been added.')
+    _console_out(f'The pair ("{term}":"{definition}") has been added.')
 
 
 def remove_card():
-    print('Which card?')
+    _console_out('Which card?')
     if (term := input()) in cards:
         del inverted[cards[term]]
         del cards[term]
-        print('The card has been removed.')
+        _console_out('The card has been removed.')
     else:
-        print(f'Can\'t remove "{term}": there is no such card.')
+        _console_out(f'Can\'t remove "{term}": there is no such card.')
 
 
 def load_cards():
-    print('File name:')
+    _console_out('File name:')
     try:
         count = 0
-        with open(input()) as file:
+        with open(_console_in()) as file:
             for line in file:
                 term, definition, error_count = line.rstrip().split(':')
                 cards[term] = definition
                 inverted[definition] = term
                 errors[term] = int(error_count)
                 count += 1
-        print(f'{count} cards have been loaded.')
+        _console_out(f'{count} cards have been loaded.')
     except FileNotFoundError:
-        print('File not found.')
+        _console_out('File not found.')
 
 
 def save_cards():
-    print('File name:')
-    with open(input(), 'w') as file:
+    _console_out('File name:')
+    with open(_console_in(), 'w') as file:
         for term, definition in cards.items():
             file.write(f'{term}:{definition}:{errors[term]}\n')
-    print(f'{len(cards)} cards have been saved.')
+    _console_out(f'{len(cards)} cards have been saved.')
 
 
 def quiz():
-    print('How many times to ask?')
+    _console_out('How many times to ask?')
     quiz_cards = choices(list(cards.items()), k=int(input()))
     for term, definition in quiz_cards:
-        print(f'Print the definition of "{term}":')
+        _console_out(f'Print the definition of "{term}":')
         if (answer := input()) == definition:
-            print('Correct!')
+            _console_out('Correct!')
         else:
             errors[term] += 1
-            print(
+            _console_out(
                 f'Wrong. The right answer is "{definition}", but your definition is correct for "{inverted[answer]}".'
                 if answer in inverted
                 else f'Wrong. The right answer is "{definition}".'
@@ -70,19 +82,21 @@ def quiz():
 
 
 def save_log():
-    print('File name:')
-    input()
-    print('The log has been saved.')
+    _console_out('File name:')
+    with open(_console_in(), 'w') as file:
+        for entry in log:
+            print(entry, file=file)
+    _console_out('The log has been saved.')
 
 
 def display_hardest():
     sorted_errors = errors.most_common()
     if not sorted_errors or sorted_errors[0][1] == 0:
-        print('There are no cards with errors.')
+        _console_out('There are no cards with errors.')
     else:
         max_errors = sorted_errors[0][1]
         hardest = [pair[0] for pair in takewhile(lambda p: p[1] == max_errors, sorted_errors)]
-        print(
+        _console_out(
             f'The hardest card is {hardest[0]}. You have {max_errors} errors answering it.'
             if len(hardest) == 1
             else f'The hardest cards are {", ".join(hardest)}. You have {max_errors} errors answering them.'
@@ -91,7 +105,7 @@ def display_hardest():
 
 def clear_errors():
     errors.clear()
-    print('Card statistics have been reset.')
+    _console_out('Card statistics have been reset.')
 
 
 def main():
@@ -106,10 +120,10 @@ def main():
         'reset stats': clear_errors,
     }
     while True:
-        print(
+        _console_out(
             '\nInput the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):'
         )
-        if (selection := input()) == 'exit':
+        if (selection := _console_in()) == 'exit':
             print('Bye bye!')
             return
         else:
