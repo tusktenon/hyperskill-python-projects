79cadec956621532bc42dbab95977223fce56a8c
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2026-02-14 08:32:57 -0500

    Password Hacker: improve Stage 2 solution

--- PasswordHacker/hack2.py
+++ PasswordHacker/hack2.py
@@ -1,29 +1,31 @@
-import itertools
-import socket
-import string
-import sys
+from itertools import count, product
+from socket import socket
+from string import ascii_lowercase, digits
+from sys import argv
 
 
-def brute_force(client):
-    chars = string.ascii_lowercase + string.digits
-    for length in range(1, 64):
-        for candidate in (''.join(t) for t in itertools.product(chars, repeat=length)):
+def brute_force_generator():
+    for length in count(1):
+        for candidate in product(ascii_lowercase + digits, repeat=length):
+            yield ''.join(candidate)
+
+
+def brute_force_hack(address):
+    with socket() as client:
+        client.connect(address)
+        for candidate in brute_force_generator():
             client.sendall(candidate.encode())
             response = client.recv(1024).decode()
             if response == 'Connection success!':
                 return candidate
-            if response == 'Too many attempts':
-                return '[Too many attempts]'
+        return None
 
 
 def main():
-    hostname, port = sys.argv[1:]
+    hostname, port = argv[1:]
     address = (hostname, int(port))
-    with socket.socket() as client:
-        client.connect(address)
-        print(brute_force(client))
+    print(brute_force_hack(address))
 
 
 if __name__ == '__main__':
     main()
-
