8035caaafd89e455eee8812ca8be62ba273b39f6
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2026-04-16 16:37:35 -0400

    Sorting Tool: clean up Stage 6 solution

--- SortingTool/stage6.py
+++ SortingTool/stage6.py
@@ -1,14 +1,8 @@
 import argparse
 from collections import Counter
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-dataType', nargs='?', default='word')
-parser.add_argument('-sortingType', nargs='?', default='natural')
-parser.add_argument('-inputFile')
-parser.add_argument('-outputFile')
 
-
-def get_elements(data_type, input_file):
+def load(data_type, input_file):
     elements = []
     skipped = []
 
@@ -42,23 +36,35 @@ def _process_line(line, elements, skipped, data_type):
             elements += line.split()
 
 
-def display_sorted(elements, data_type, sort_type, output_file):
-    dst = open(output_file, 'w') if output_file else None
+def report(elements, data_type, sort_type, output_file):
+    if output_file:
+        with open(output_file, 'w') as dst:
+            _print_sorted(elements, data_type, sort_type, dst)
+    else:
+        _print_sorted(elements, data_type, sort_type)
+
+
+def _print_sorted(elements, data_type, sort_type, dst=None):
     display_name = {'line': 'lines', 'long': 'numbers', 'word': 'words'}[data_type]
     print(f'Total {display_name}: {len(elements)}.', file=dst)
     if sort_type == 'natural':
         elements.sort()
-        print('Sorted data:', *elements, sep='\n' if data_type == 'line' else ' ', file=dst)
+        sep = '\n' if data_type == 'line' else ' '
+        print('Sorted data:', *elements, sep=sep, file=dst)
     else:
         counts = list(Counter(elements).items())
         counts.sort(key=lambda item: (item[1], item[0]))
         for element, count in counts:
             print(f'{element}: {count} time(s), {100 * count // len(elements)}%', file=dst)
-    if dst:
-        dst.close()
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-dataType', nargs='?', default='word')
+    parser.add_argument('-sortingType', nargs='?', default='natural')
+    parser.add_argument('-inputFile')
+    parser.add_argument('-outputFile')
+
     args, invalid = parser.parse_known_args()
     for arg in invalid:
         print(f'"{arg}" is not a valid parameter. It will be skipped.')
@@ -68,8 +74,9 @@ def main():
     if args.sortingType is None:
         print('No sorting type defined!')
         exit(1)
-    elements = get_elements(args.dataType, args.inputFile)
-    display_sorted(elements, args.dataType, args.sortingType, args.outputFile)
+
+    elements = load(args.dataType, args.inputFile)
+    report(elements, args.dataType, args.sortingType, args.outputFile)
 
 
 if __name__ == '__main__':
